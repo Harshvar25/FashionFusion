0341273f91305abe97053c6ce7000e317af46d2a
Harshvar25 <harshvardhansingh.bais2021@vitstudent.ac.in>
2023-09-10 22:40:46 +0530

    asd

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
       <Router basename="/FashionFusion">
         <GlobalStyle />
         <Header />
-        <Routes>
+        <Routes basename="/FashionFusion">
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/products" element={<Products />} />
